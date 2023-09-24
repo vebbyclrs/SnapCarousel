944c4d63fe48a0164c467458d16f9bb87f059b58
Vebby Clarissa <vebby.clarissa@aleph-labs.com>
2023-09-24 12:19:49 +0700

    Initial Commit

---
+++ SnapCarouselSlider/SnapCarouselSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapCarouselSliderApp.swift
+//  SnapCarouselSlider
+//
+//  Created by Aleph-9Q05D on 24/09/23.
+//
+
+import SwiftUI
+
+@main
+struct SnapCarouselSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
